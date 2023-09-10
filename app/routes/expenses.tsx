2f1de80813ccb625c8b9386244dfb74d54f500d2
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-10 15:05:36 +0100

    add components and styles

--- app/routes/expenses.tsx
+++ app/routes/expenses.tsx
@@ -1,3 +1,15 @@
-export default function ExpensesPage() {
-    return <h1>Expenses Page</h1>
+import type { LinksFunction } from "@remix-run/node";
+
+import { Outlet } from "@remix-run/react";
+import expensesStyles from '../styles/expenses.css'
+
+export default function ExpensesLayout() {
+    return <main>
+        <p>Shared Element!</p>
+        <Outlet />
+    </main>
+}
+
+export const links: LinksFunction = () => {
+    return [{ rel: 'stylesheet', href: expensesStyles }]
 }

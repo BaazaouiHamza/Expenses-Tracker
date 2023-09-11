f030e5f9a101bb146d1b11a03486ca5e8200a0b1
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-12 00:31:56 +0100

    add getExpense and initial value for edit expense

--- app/components/expenses/ExpenseForm.tsx
+++ app/components/expenses/ExpenseForm.tsx
@@ -1,10 +1,26 @@
-import { Form, Link, useActionData, useNavigation } from "@remix-run/react";
+import { Form, Link, useActionData, useLoaderData, useNavigation } from "@remix-run/react";
 import type { ValidationErrors } from "~/data/validation.server";
 
+
 function ExpenseForm() {
+    const defaultExpense = {
+        title: "",
+        amount: "",
+        date: "",
+    }
+
     const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
+
     const validationErrors = useActionData() as ValidationErrors
+
     const navigation = useNavigation()
+
+    const expenseData = useLoaderData()
+    const defaultValues = expenseData ? {
+        title: expenseData.title,
+        amount: expenseData.amount,
+        date: expenseData.date
+    } : defaultExpense
 
     const isSubmitting = navigation.state !== "idle"
     // const submit = useSubmit()
@@ -28,7 +44,7 @@ function ExpenseForm() {
         >
             <p>
                 <label htmlFor="title">Expense Title</label>
-                <input type="text" id="title" name="title" required maxLength={30} />
+                <input type="text" id="title" name="title" required maxLength={30} defaultValue={defaultValues.title} />
             </p>
 
             <div className="form-row">
@@ -41,11 +57,19 @@ function ExpenseForm() {
                         min="0"
                         step="0.01"
                         required
+                        defaultValue={defaultValues.amount}
                     />
                 </p>
                 <p>
                     <label htmlFor="date">Date</label>
-                    <input type="date" id="date" name="date" max={today} required />
+                    <input
+                        type="date"
+                        id="date"
+                        name="date"
+                        max={today}
+                        required
+                        defaultValue={defaultValues.date ? defaultValues.date.slice(0, 10) : ''}
+                    />
                 </p>
             </div>
             {validationErrors &&
--- app/routes/__app/expenses/$id.tsx
+++ app/routes/__app/expenses/$id.tsx
@@ -1,6 +1,8 @@
+import type { LoaderFunction } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { getExpense } from "~/data/expenses.server";
 
 export default function UpdateExpensesPage() {
     const navigate = useNavigate()
@@ -10,4 +12,8 @@ export default function UpdateExpensesPage() {
         navigate('..')
     }
     return <Modal onClose={closeHandler} children={<ExpenseForm />} />
+}
+
+export const loader: LoaderFunction = ({ params }) => {
+    return getExpense(params.id)
 }

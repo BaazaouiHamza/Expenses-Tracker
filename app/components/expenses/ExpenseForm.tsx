c7323262978042536336a124a9f801f9ebacc5c5
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-11 23:32:12 +0100

    show indicator when user is submitting an expense

--- app/components/expenses/ExpenseForm.tsx
+++ app/components/expenses/ExpenseForm.tsx
@@ -1,11 +1,31 @@
-import { Link, useActionData } from "@remix-run/react";
+import { Form, Link, useActionData, useNavigation } from "@remix-run/react";
 import type { ValidationErrors } from "~/data/validation.server";
 
 function ExpenseForm() {
     const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
     const validationErrors = useActionData() as ValidationErrors
+    const navigation = useNavigation()
+
+    const isSubmitting = navigation.state !== "idle"
+    // const submit = useSubmit()
+    // const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    //     event.preventDefault()
+    //     // perform your own validation
+    //     // ...
+
+    //     submit(event.currentTarget, {
+    //         // action: '/expense/add',
+    //         method: 'post'
+    //     })
+    // }
+
     return (
-        <form method="post" className="form" id="expense-form">
+        <Form
+            method="post"
+            className="form"
+            id="expense-form"
+        // onSubmit={submitHandler}
+        >
             <p>
                 <label htmlFor="title">Expense Title</label>
                 <input type="text" id="title" name="title" required maxLength={30} />
@@ -33,10 +53,10 @@ function ExpenseForm() {
                     {Object.values(validationErrors).map(error => <li key={error}>{error}</li>)}
                 </ul>}
             <div className="form-actions">
-                <button>Save Expense</button>
+                <button disabled={isSubmitting}>{isSubmitting ? 'Saving...' : 'Save Expense'}</button>
                 <Link to="..">Cancel</Link>
             </div>
-        </form>
+        </Form>
     );
 }
 

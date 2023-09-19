19ed4e98808746179edd82bbbfb9ae6db22c4bc1
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-19 14:50:49 +0100

    add confirm pop up before deleting an expense

--- app/components/expenses/ExpenseListItem.tsx
+++ app/components/expenses/ExpenseListItem.tsx
@@ -1,9 +1,23 @@
-import { Form, Link } from "@remix-run/react";
+import { Link, useFetcher } from "@remix-run/react";
 
 function ExpenseListItem({ id, title, amount }) {
-    // function deleteExpenseItemHandler() {
-    //     // tbd
-    // }
+    const fetcher = useFetcher()
+
+    // const submit = useSubmit()
+    function deleteExpenseItemHandler() {
+        const proceed = confirm('Are you sure? Do you want to delete this item?')
+        // submit(null, { method: 'delete', action: `/expenses/${id}` })
+        if (!proceed) {
+            return
+        }
+        fetcher.submit(null, { method: 'delete', action: `/expenses/${id}` })
+    }
+
+    if (fetcher.state !== 'idle') {
+        return <article className="expense-item locker">
+            <p>Deleting...</p>
+        </article>
+    }
 
     return (
         <article className="expense-item">
@@ -12,10 +26,10 @@ function ExpenseListItem({ id, title, amount }) {
                 <p className="expense-amount">${amount.toFixed(2)}</p>
             </div>
             <menu className="expense-actions">
-                {/* <button onClick={deleteExpenseItemHandler}>Delete</button> */}
-                <Form method='delete' action={`/expenses/${id}`}>
+                <button onClick={deleteExpenseItemHandler}>Delete</button>
+                {/* <Form method='delete' action={`/expenses/${id}`}>
                     <button>Delete</button>
-                </Form>
+                </Form> */}
 
                 <Link to={id}>Edit</Link>
             </menu >

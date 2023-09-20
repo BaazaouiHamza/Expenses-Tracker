ce9fd95e2e228fa58398a3932f956ef2a38ce563
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-09-20 13:19:39 +0100

    relay real data via loader to expenses.analaysis component

--- app/routes/__app/expenses.analysis.tsx
+++ app/routes/__app/expenses.analysis.tsx
@@ -1,13 +1,40 @@
+import { json, type LoaderFunction } from "@remix-run/node";
+import { isRouteErrorResponse, useLoaderData, useRouteError } from "@remix-run/react";
 import Chart from "~/components/expenses/Chart";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
-import { Dummy_Expenses } from "~/types/Expense";
+import ErrorComponent from "~/components/util/Error";
+import { getExpenses } from "~/data/expenses.server";
 
 
 
 
 export default function ExpensesAnalysisPage() {
-    return <main>
-        <Chart expenses={Dummy_Expenses} />
-        <ExpenseStatistics expenses={Dummy_Expenses} />
-    </main>
+    const expenses = useLoaderData()
+
+    return (<main>
+        <Chart expenses={expenses} />
+        <ExpenseStatistics expenses={expenses} />
+    </main>)
+}
+
+export const loader: LoaderFunction = async () => {
+    const expenses = await getExpenses()
+    if (!expenses || expenses.length === 0) {
+        throw json(
+            { message: 'Could not load expenses for the requested analysis' },
+            { status: 404, statusText: "No expenses found." })
+    }
+    return expenses
+}
+
+export const ErrorBoundary = () => {
+    const error = useRouteError()
+
+    if (isRouteErrorResponse(error)) {
+        return <main>
+            <ErrorComponent title={error.statusText}>
+                <p>{error.data?.message || 'Something went wrong - could not load expenses.'}</p>
+            </ErrorComponent>
+        </main>
+    }
 }
